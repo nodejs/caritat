b129d422189fb3de6f79cf289fdbe608d5b061c7
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-06-13 11:50:36 +0200

    Apply suggestions from code review

--- packages/core/src/utils/cleanMarkdown.ts
+++ packages/core/src/utils/cleanMarkdown.ts
@@ -5,11 +5,11 @@ function cleanUnsupportedMarkdown(txt: string): string {
 
 function reduceToFirstLine(candidate: string) {
   const i = candidate.indexOf('\n')
-  return i === -1 ? candidate : candidate.slice(0, i - Number(candidate[i] === '.' || candidate === ':'));
+  return i === -1 ? candidate : candidate.slice(0, i);
 }
 export default function cleanMarkdown(txt: string, keepOnlyFirstLineInSummary?: boolean): string {
   if (keepOnlyFirstLineInSummary) txt = reduceToFirstLine(txt);
-  else if (txt.endsWith('.') || txt.endsWith(':')) txt = txt.slice(0, -1);
+  if (txt.endsWith('.') || txt.endsWith(':')) txt = txt.slice(0, -1);
 
   // Escape backticks for edge case scenarii (no code span support).
   if (txt.includes("``") || txt.includes("\\`")) {
